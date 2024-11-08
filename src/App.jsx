f78ab6b8e1e73b781f2e47e2867a808f9f377eff
pinK0der <obsidianfury64@gmail.com>
2024-11-08 19:52:26 +0100

    20px

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './components/Login'
+import Title from './components/Title'
 
 function App() {
   const [count, setCount] = useState(0)
+  
 
   return (
     <>
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Neque harum iusto distinctio, eius error quasi animi exercitationem nihil cupiditate cumque? Optio adipisci illo quasi possimus dignissimos voluptates quibusdam nemo porro?</p>
+      <Title />
       <Login />
     </>
   )
