8ee859793ffbde9a2cc50843319f55fded2af0a6
Artem Polianetskyi <artem.polianetskyi@student.tuke.sk>
2024-11-08 18:59:54 +0100

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ function App() {
 
   return (
     <>
+    <h1 className='title greeting_title'>Welcome to the Future View</h1>
+    <p className='text greeting_text'>Future you in your hands</p>
       <Login />
     </>
   )
